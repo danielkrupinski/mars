f5a4d43784baec737c48aa4ac540b44580eeaac4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-06 15:44:41 +0100

    Update formatting

--- src/Utils/util_items.h
+++ src/Utils/util_items.h
@@ -8,30 +8,30 @@
 
 namespace Util
 {
-	namespace Items
-	{
-		/*
-		 * Look up a ItemDefinitionIndex ordinal value by the symbolic name
-		 * using an internal map
-		 */
-		enum ItemDefinitionIndex GetItemIndex(const std::string);
+    namespace Items
+    {
+        /*
+        * Look up a ItemDefinitionIndex ordinal value by the symbolic name
+        * using an internal map
+        */
+        enum ItemDefinitionIndex GetItemIndex(const std::string);
 
-		/*
-		 * Look up the symbolic name of a ItemDefinitionIndex ordinal value
-		 * using an internal map
-		 */
-		std::string GetItemName(const enum ItemDefinitionIndex);
+        /*
+        * Look up the symbolic name of a ItemDefinitionIndex ordinal value
+        * using an internal map
+        */
+        std::string GetItemName(const enum ItemDefinitionIndex);
 
-		std::string GetItemEntityName(ItemDefinitionIndex index);
+        std::string GetItemEntityName(ItemDefinitionIndex index);
 
-		std::string GetItemConfigEntityName(ItemDefinitionIndex index);
+        std::string GetItemConfigEntityName(ItemDefinitionIndex index);
 
-		std::string GetItemDisplayName(ItemDefinitionIndex index);
+        std::string GetItemDisplayName(ItemDefinitionIndex index);
 
-		bool IsKnife(ItemDefinitionIndex index);
-		bool IsUtility(ItemDefinitionIndex index);
-		bool IsGlove(ItemDefinitionIndex index);
-		bool IsCTWeapon(ItemDefinitionIndex index);
-		bool IsTWeapon(ItemDefinitionIndex index);
-	}
+        bool IsKnife(ItemDefinitionIndex index);
+        bool IsUtility(ItemDefinitionIndex index);
+        bool IsGlove(ItemDefinitionIndex index);
+        bool IsCTWeapon(ItemDefinitionIndex index);
+        bool IsTWeapon(ItemDefinitionIndex index);
+    }
 }
