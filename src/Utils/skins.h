67ea7eedcfc225cfd2fb3d8a3ff9a955b30bbdfa
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-07 18:07:03 +0100

    Add 'Moto Gloves | POW! ' to skinchanger

--- src/Utils/skins.h
+++ src/Utils/skins.h
@@ -661,6 +661,7 @@ const std::map<int, PaintNames_t> itemSkins = {
     { 10046, { "#PaintKit_sporty_poison_frog_red_green_tag", "sporty_poison_frog_red_green" } },
     { 10047, { "#PaintKit_sporty_black_webbing_yellow_tag", "sporty_black_webbing_yellow" } },
     { 10048, { "#PaintKit_sporty_blue_pink_tag", "sporty_blue_pink" } },
+    { 10049, { "#PaintKit_motorcycle_choco_boom_tag", "motorcycle_choco_boom" } },
     { 10050, { "#PaintKit_motorcycle_basic_green_orange_tag", "motorcycle_basic_green_orange" } },
     { 10051, { "#PaintKit_motorcycle_yellow_camo_tag", "motorcycle_yellow_camo" } },
     { 10052, { "#PaintKit_motorcycle_trigrid_blue_tag", "motorcycle_trigrid_blue" } },
