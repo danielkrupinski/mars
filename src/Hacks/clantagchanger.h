8a0a707abf88e3b7438c0d28924fddb2bd6ea17f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 15:47:11 +0100

    Update formatting

--- src/Hacks/clantagchanger.h
+++ src/Hacks/clantagchanger.h
@@ -7,60 +7,60 @@
 
 namespace ClanTagChanger
 {
-	struct Frame
-	{
-		int time;
-		std::wstring text;
+    struct Frame
+    {
+        int time;
+        std::wstring text;
 
-		Frame(std::wstring text, int time)
-		{
-			this->text = text;
-			this->time = time;
-		}
-	};
+        Frame(std::wstring text, int time)
+        {
+            this->text = text;
+            this->time = time;
+        }
+    };
 
-	enum AnimationLoopType
-	{
-		ANIM_LOOP,
-		ANIM_LOOP_BACK
-	};
+    enum AnimationLoopType
+    {
+        ANIM_LOOP,
+        ANIM_LOOP_BACK
+    };
 
-	struct Animation
-	{
-		std::string name;
-		unsigned int currentFrame;
-		std::vector<Frame> frames;
-		AnimationLoopType loopType;
+    struct Animation
+    {
+        std::string name;
+        unsigned int currentFrame;
+        std::vector<Frame> frames;
+        AnimationLoopType loopType;
 
-		Animation(std::string name, std::vector<Frame> frames, AnimationLoopType loopType)
-		{
-			currentFrame = 0;
-			this->name = name;
-			this->frames = frames;
-			this->loopType = loopType;
-		}
+        Animation(std::string name, std::vector<Frame> frames, AnimationLoopType loopType)
+        {
+            currentFrame = 0;
+            this->name = name;
+            this->frames = frames;
+            this->loopType = loopType;
+        }
 
-		Frame GetCurrentFrame()
-		{
-			return frames[currentFrame];
-		}
+        Frame GetCurrentFrame()
+        {
+            return frames[currentFrame];
+        }
 
-		void NextFrame()
-		{
-			currentFrame++;
+        void NextFrame()
+        {
+            currentFrame++;
 
-			if (currentFrame >= frames.size())
-				currentFrame = 0;
-		}
-	};
+            if (currentFrame >= frames.size())
+            currentFrame = 0;
+        }
+    };
 
-	extern std::vector<Animation> animations;
-	extern Animation* animation;
+    extern std::vector<Animation> animations;
+    extern Animation* animation;
 
-	void UpdateClanTagCallback();
+    void UpdateClanTagCallback();
 
-	//Hooks
-	void BeginFrame(float frameTime);
+    //Hooks
+    void BeginFrame(float frameTime);
 }
 
 extern SendClanTagFn SendClanTag;
