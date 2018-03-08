a1c1788a74962c572b941e724b5a561f5f9489f1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 16:26:10 +0100

    Update formatting

--- src/Hacks/skybox.cpp
+++ src/Hacks/skybox.cpp
@@ -10,85 +10,85 @@ std::unordered_map<MaterialHandle_t, ImColor> skyboxMaterials;
 std::unordered_map<MaterialHandle_t, ImColor> skyboxMaterials2;
 
 const char* skyBoxNames[] = {
-		"cs_baggage_skybox_", // 0
-		"cs_tibet", "embassy",
-		"italy",
-		"jungle",
-		"office",
-		"nukeblank",
-		"sky_venice",
-		"sky_cs15_daylight01_hdr",
-		"sky_cs15_daylight02_hdr",
-		"sky_cs15_daylight03_hdr",
-		"sky_cs15_daylight04_hdr",
-		"sky_csgo_cloudy01",
-		"sky_csgo_night_flat",
-		"sky_csgo_night02",
-		"sky_csgo_night02b",
-		"sky_day02_05",
-		"sky_day02_05_hdr",
-		"sky_dust",
-		"vertigo",
-		"vertigo_hdr",
-		"vertigoblue_hdr",
-		"vietnam" // 21
+    "cs_baggage_skybox_", // 0
+    "cs_tibet", "embassy",
+    "italy",
+    "jungle",
+    "office",
+    "nukeblank",
+    "sky_venice",
+    "sky_cs15_daylight01_hdr",
+    "sky_cs15_daylight02_hdr",
+    "sky_cs15_daylight03_hdr",
+    "sky_cs15_daylight04_hdr",
+    "sky_csgo_cloudy01",
+    "sky_csgo_night_flat",
+    "sky_csgo_night02",
+    "sky_csgo_night02b",
+    "sky_day02_05",
+    "sky_day02_05_hdr",
+    "sky_dust",
+    "vertigo",
+    "vertigo_hdr",
+    "vertigoblue_hdr",
+    "vietnam" // 21
 };
 
 void SkyBox::FrameStageNotify(ClientFrameStage_t stage)
 {
-	if (!engine->IsInGame() && skyboxMaterials.size() > 0)
-	{
-		for (const auto& it: skyboxMaterials)
-		{
-			IMaterial* mat = material->GetMaterial(it.first);
+    if (!engine->IsInGame() && skyboxMaterials.size() > 0)
+    {
+        for (const auto& it: skyboxMaterials)
+        {
+            IMaterial* mat = material->GetMaterial(it.first);
 
-			if (!mat)
-				continue;
+            if (!mat)
+            continue;
 
-			mat->GetColorModulate(&r1, &g1, &b1);
-			a1 = mat->GetAlphaModulation();
+            mat->GetColorModulate(&r1, &g1, &b1);
+            a1 = mat->GetAlphaModulation();
 
-			mat->ColorModulate(r1, g1, b1);
-			mat->AlphaModulate(a1);
-		}
+            mat->ColorModulate(r1, g1, b1);
+            mat->AlphaModulate(a1);
+        }
 
-		skyboxMaterials.clear();
-		skyboxMaterials2.clear();
-	}
+        skyboxMaterials.clear();
+        skyboxMaterials2.clear();
+    }
 
-	if( engine->IsInGame() && Settings::ESP::enabled && Settings::SkyBox::enabled )
-	{
-		SetNamedSkyBox(skyBoxNames[Settings::SkyBox::skyBoxNumber]); // Thanks to @Flawww
-		return;
-	}
+    if( engine->IsInGame() && Settings::ESP::enabled && Settings::SkyBox::enabled )
+    {
+        SetNamedSkyBox(skyBoxNames[Settings::SkyBox::skyBoxNumber]); // Thanks to @Flawww
+        return;
+    }
 
-	if (stage != ClientFrameStage_t::FRAME_NET_UPDATE_POSTDATAUPDATE_END)
-		return;
+    if (stage != ClientFrameStage_t::FRAME_NET_UPDATE_POSTDATAUPDATE_END)
+    return;
 
-	for (MaterialHandle_t i = material->FirstMaterial(); i != material->InvalidMaterial(); i = material->NextMaterial(i))
-	{
-		IMaterial* mat = material->GetMaterial(i);
+    for (MaterialHandle_t i = material->FirstMaterial(); i != material->InvalidMaterial(); i = material->NextMaterial(i))
+    {
+        IMaterial* mat = material->GetMaterial(i);
 
-		if (!mat || strcmp(mat->GetTextureGroupName(), TEXTURE_GROUP_SKYBOX) != 0)
-			continue;
+        if (!mat || strcmp(mat->GetTextureGroupName(), TEXTURE_GROUP_SKYBOX) != 0)
+        continue;
 
-		if (skyboxMaterials.find(i) == skyboxMaterials.end())
-		{
-			mat->GetColorModulate(&r1, &g1, &b1);
-			a1 = mat->GetAlphaModulation();
-			skyboxMaterials.emplace(i, ImColor(r1, g1, b1, a1));
-			skyboxMaterials2.emplace(i, ImColor(r1, g1, b1, a1));
-		}
+        if (skyboxMaterials.find(i) == skyboxMaterials.end())
+        {
+            mat->GetColorModulate(&r1, &g1, &b1);
+            a1 = mat->GetAlphaModulation();
+            skyboxMaterials.emplace(i, ImColor(r1, g1, b1, a1));
+            skyboxMaterials2.emplace(i, ImColor(r1, g1, b1, a1));
+        }
 
-		ImColor color = (Settings::NoSky::enabled && Settings::ESP::enabled && !Settings::SkyBox::enabled) ? Settings::NoSky::color.Color() : skyboxMaterials2.find(i)->second;
+        ImColor color = (Settings::NoSky::enabled && Settings::ESP::enabled && !Settings::SkyBox::enabled) ? Settings::NoSky::color.Color() : skyboxMaterials2.find(i)->second;
 
-		if (skyboxMaterials.at(i) != color)
-		{
-			mat->ColorModulate(color);
+        if (skyboxMaterials.at(i) != color)
+        {
+            mat->ColorModulate(color);
 
-			skyboxMaterials.at(i) = color;
-		}
-	}
+            skyboxMaterials.at(i) = color;
+        }
+    }
 
 
 }
