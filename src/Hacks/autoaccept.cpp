b842ef8ec3b20c9848d42af46e0dbf48c261dd1b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 15:05:17 +0100

    Update formatting

--- src/Hacks/autoaccept.cpp
+++ src/Hacks/autoaccept.cpp
@@ -4,20 +4,20 @@ bool Settings::AutoAccept::enabled = false;
 
 /* This doesn't matter too much, it just has to be big enough for the game to write various parts of it */
 struct CServerConfirmedReservationCheckCallback {
-	char pad[0x12788];
+    char pad[0x12788];
 };
 
 void AutoAccept::PlaySound(const char* filename)
 {
-	if (!Settings::AutoAccept::enabled)
-		return;
+    if (!Settings::AutoAccept::enabled)
+    return;
 
-	if (engine->IsInGame())
-		return;
+    if (engine->IsInGame())
+    return;
 
-	if (strcmp(filename, XORSTR("!UI/competitive_accept_beep.wav")) != 0)
-		return;
+    if (strcmp(filename, XORSTR("!UI/competitive_accept_beep.wav")) != 0)
+    return;
 
-	CServerConfirmedReservationCheckCallback empty_callback;
-	IsReadyCallback(&empty_callback);
+    CServerConfirmedReservationCheckCallback empty_callback;
+    IsReadyCallback(&empty_callback);
 }
