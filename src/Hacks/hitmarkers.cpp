ac28f1d5121984da3eb210980718bcb694d0b77f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 16:02:40 +0100

    Update formatting

--- src/Hacks/hitmarkers.cpp
+++ src/Hacks/hitmarkers.cpp
@@ -15,100 +15,100 @@ long lastHitmarkerTimestamp = 0;
 
 void Hitmarkers::Paint()
 {
-	if (!Settings::ESP::enabled || !Settings::ESP::Hitmarker::enabled)
-		return;
+    if (!Settings::ESP::enabled || !Settings::ESP::Hitmarker::enabled)
+    return;
 
-	if (!engine->IsInGame())
-		return;
+    if (!engine->IsInGame())
+    return;
 
-	C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
-	if ( !localplayer || !localplayer->GetAlive() )
-		return;
+    C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
+    if ( !localplayer || !localplayer->GetAlive() )
+    return;
 
-	int duration = Settings::ESP::Hitmarker::duration;
-	long now = Util::GetEpochTime();
+    int duration = Settings::ESP::Hitmarker::duration;
+    long now = Util::GetEpochTime();
 
-	long diff = lastHitmarkerTimestamp + duration - now;
-	if (diff <= 0)
-		return;
+    long diff = lastHitmarkerTimestamp + duration - now;
+    if (diff <= 0)
+    return;
 
-	int width, height;
-	engine->GetScreenSize(width, height);
+    int width, height;
+    engine->GetScreenSize(width, height);
 
-	Color color = Color::FromImColor(Settings::ESP::Hitmarker::color.Color());
-	color.a = std::min(color.a, (int)(diff * color.a / duration * 2));
+    Color color = Color::FromImColor(Settings::ESP::Hitmarker::color.Color());
+    color.a = std::min(color.a, (int)(diff * color.a / duration * 2));
 
-	int sides[4][2] = { {-1, -1}, {1, 1}, {-1, 1}, {1, -1} };
-	for (auto& it : sides)
-		Draw::Line(width / 2 + (Settings::ESP::Hitmarker::innerGap * it[0]), height / 2 + (Settings::ESP::Hitmarker::innerGap * it[1]), width / 2 + (Settings::ESP::Hitmarker::size * it[0]), height / 2 + (Settings::ESP::Hitmarker::size * it[1]), color);
+    int sides[4][2] = { {-1, -1}, {1, 1}, {-1, 1}, {1, -1} };
+    for (auto& it : sides)
+    Draw::Line(width / 2 + (Settings::ESP::Hitmarker::innerGap * it[0]), height / 2 + (Settings::ESP::Hitmarker::innerGap * it[1]), width / 2 + (Settings::ESP::Hitmarker::size * it[0]), height / 2 + (Settings::ESP::Hitmarker::size * it[1]), color);
 
-	if (!Settings::ESP::Hitmarker::Damage::enabled)
-		return;
+    if (!Settings::ESP::Hitmarker::Damage::enabled)
+    return;
 
-	float textHeight = Draw::GetTextSize(XORSTR("[cool]"), esp_font).y;
+    float textHeight = Draw::GetTextSize(XORSTR("[cool]"), esp_font).y;
 
-	for (unsigned int i = 0; i < damages.size(); i++)
-	{
-		long timestamp = damages[i].second;
-		long hitDiff = timestamp + duration - now;
+    for (unsigned int i = 0; i < damages.size(); i++)
+    {
+        long timestamp = damages[i].second;
+        long hitDiff = timestamp + duration - now;
 
-		if (hitDiff <= 0)
-		{
-			damages.erase(damages.begin() + i);
-			continue;
-		}
+        if (hitDiff <= 0)
+        {
+            damages.erase(damages.begin() + i);
+            continue;
+        }
 
-		Vector2D pos = Vector2D(
-				width / 2 + Settings::ESP::Hitmarker::size + 4,
-				height / 2 - Settings::ESP::Hitmarker::size - textHeight * i + 4
-		);
+        Vector2D pos = Vector2D(
+            width / 2 + Settings::ESP::Hitmarker::size + 4,
+            height / 2 - Settings::ESP::Hitmarker::size - textHeight * i + 4
+        );
 
-		int damage = damages[i].first;
-		std::string damageStr = '-' + std::to_string(damage);
+        int damage = damages[i].first;
+        std::string damageStr = '-' + std::to_string(damage);
 
-		color.a = Color::FromImColor(Settings::ESP::Hitmarker::color.Color()).a;
-		color.a = std::min(color.a, (int)(hitDiff * color.a / duration * 2));
+        color.a = Color::FromImColor(Settings::ESP::Hitmarker::color.Color()).a;
+        color.a = std::min(color.a, (int)(hitDiff * color.a / duration * 2));
 
-		Draw::Text(pos, damageStr.c_str(), esp_font, color);
-	}
+        Draw::Text(pos, damageStr.c_str(), esp_font, color);
+    }
 }
 
 void Hitmarkers::FireGameEvent(IGameEvent* event)
 {
-	if (!Settings::ESP::Hitmarker::enabled)
-		return;
+    if (!Settings::ESP::Hitmarker::enabled)
+    return;
 
-	if (!engine->IsInGame())
-		return;
+    if (!engine->IsInGame())
+    return;
 
-	if (strcmp(event->GetName(), XORSTR("player_hurt")) != 0)
-		return;
+    if (strcmp(event->GetName(), XORSTR("player_hurt")) != 0)
+    return;
 
-	int hurt_player_id = event->GetInt(XORSTR("userid"));
-	int attacker_id = event->GetInt(XORSTR("attacker"));
+    int hurt_player_id = event->GetInt(XORSTR("userid"));
+    int attacker_id = event->GetInt(XORSTR("attacker"));
 
-	if (engine->GetPlayerForUserID(hurt_player_id) == engine->GetLocalPlayer())
-		return;
+    if (engine->GetPlayerForUserID(hurt_player_id) == engine->GetLocalPlayer())
+    return;
 
-	// TODO: show hitmarks while spectating?
-	if (engine->GetPlayerForUserID(attacker_id) != engine->GetLocalPlayer())
-		return;
+    // TODO: show hitmarks while spectating?
+    if (engine->GetPlayerForUserID(attacker_id) != engine->GetLocalPlayer())
+    return;
 
-	C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
-	if (!localplayer)
-		return;
+    C_BasePlayer* localplayer = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
+    if (!localplayer)
+    return;
 
-	C_BasePlayer* hurt_player = (C_BasePlayer*) entityList->GetClientEntity(engine->GetPlayerForUserID(hurt_player_id));
-	if (!hurt_player)
-		return;
+    C_BasePlayer* hurt_player = (C_BasePlayer*) entityList->GetClientEntity(engine->GetPlayerForUserID(hurt_player_id));
+    if (!hurt_player)
+    return;
 
-	if (hurt_player->GetTeam() == localplayer->GetTeam() && !Settings::ESP::Hitmarker::allies)
-		return;
+    if (hurt_player->GetTeam() == localplayer->GetTeam() && !Settings::ESP::Hitmarker::allies)
+    return;
 
-	if (hurt_player->GetTeam() != localplayer->GetTeam() && !Settings::ESP::Hitmarker::enemies)
-		return;
+    if (hurt_player->GetTeam() != localplayer->GetTeam() && !Settings::ESP::Hitmarker::enemies)
+    return;
 
-	long now = Util::GetEpochTime();
-	lastHitmarkerTimestamp = now;
-	damages.insert(damages.begin(), std::pair<int, long>(event->GetInt(XORSTR("dmg_health")), now));
+    long now = Util::GetEpochTime();
+    lastHitmarkerTimestamp = now;
+    damages.insert(damages.begin(), std::pair<int, long>(event->GetInt(XORSTR("dmg_health")), now));
 }
