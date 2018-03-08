8e614df6c342d6033471bb6b2e02996a5a98697f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 16:37:52 +0100

    Update formatting

--- src/Hacks/valvedscheck.cpp
+++ src/Hacks/valvedscheck.cpp
@@ -4,14 +4,14 @@ bool ValveDSCheck::forceUT = false;
 
 void ValveDSCheck::FireGameEvent(IGameEvent* event)
 {
-	if (!event)
-		return;
+    if (!event)
+    return;
 
-	if (strcmp(event->GetName(), XORSTR("player_connect_full")) != 0 && strcmp(event->GetName(), XORSTR("cs_game_disconnected")) != 0)
-		return;
+    if (strcmp(event->GetName(), XORSTR("player_connect_full")) != 0 && strcmp(event->GetName(), XORSTR("cs_game_disconnected")) != 0)
+    return;
 
-	if (event->GetInt(XORSTR("userid")) && engine->GetPlayerForUserID(event->GetInt(XORSTR("userid"))) != engine->GetLocalPlayer())
-		return;
+    if (event->GetInt(XORSTR("userid")) && engine->GetPlayerForUserID(event->GetInt(XORSTR("userid"))) != engine->GetLocalPlayer())
+    return;
 
-	ValveDSCheck::forceUT = false;
+    ValveDSCheck::forceUT = false;
 }
