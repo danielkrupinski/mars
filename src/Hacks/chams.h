80d15422bbdf9093770c815d884a265a568b065a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 15:46:08 +0100

    Update formatting

--- src/Hacks/chams.h
+++ src/Hacks/chams.h
@@ -6,6 +6,6 @@
 
 namespace Chams
 {
-	//Hooks
-	void DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld);
+    //Hooks
+    void DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld);
 }
