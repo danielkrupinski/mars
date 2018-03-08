9b6785d71b882e982325beae0b01da8611f52de4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-08 16:39:17 +0100

    Update formatting

--- src/Hacks/view.h
+++ src/Hacks/view.h
@@ -5,7 +5,7 @@
 
 namespace View
 {
-	//Hooks
-	void FrameStageNotify(ClientFrameStage_t stage);
-	void PostFrameStageNotify(ClientFrameStage_t stage);
+    //Hooks
+    void FrameStageNotify(ClientFrameStage_t stage);
+    void PostFrameStageNotify(ClientFrameStage_t stage);
 }
