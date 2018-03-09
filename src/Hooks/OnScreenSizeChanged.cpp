bbb12b5e0ce683af8e0f620a68f99283952a99a7
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:46:44 +0100

    Update formatting

--- src/Hooks/OnScreenSizeChanged.cpp
+++ src/Hooks/OnScreenSizeChanged.cpp
@@ -2,7 +2,7 @@
 
 void Hooks::OnScreenSizeChanged(void* thisptr, int oldwidth, int oldheight)
 {
-	surfaceVMT->GetOriginalMethod<OnScreenSizeChangedFn>(116)(thisptr, oldwidth, oldheight);
+    surfaceVMT->GetOriginalMethod<OnScreenSizeChangedFn>(116)(thisptr, oldwidth, oldheight);
 
-	Fonts::SetupFonts();
+    Fonts::SetupFonts();
 }
