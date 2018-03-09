cf00ce0d09e8a718a50598c55c2533c1657b8722
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 18:06:23 +0100

    Update formatting

--- src/SDK/CUtlVector.h
+++ src/SDK/CUtlVector.h
@@ -2,20 +2,20 @@
 
 template <class T, class A = CUtlMemory<T>> class CUtlVector
 {
-	typedef A CAllocator;
+    typedef A CAllocator;
 
-	public:
-		T& operator[](int i)
-		{
-			return m_Memory[i];
-		}
+public:
+    T& operator[](int i)
+    {
+        return m_Memory[i];
+    }
 
-		int Count() const
-		{
-			return m_Size;
-		}
-	protected:
-		CAllocator m_Memory;
-		int m_Size;
-		T* m_pElements;
+    int Count() const
+    {
+        return m_Size;
+    }
+protected:
+    CAllocator m_Memory;
+    int m_Size;
+    T* m_pElements;
 };
