b976fe8d5f9e2e2c82a5c86ad2d2333505ffe527
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:41:29 +0100

    Update formatting

--- src/Hooks/DrawModelExecute.cpp
+++ src/Hooks/DrawModelExecute.cpp
@@ -2,16 +2,16 @@
 
 void Hooks::DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld)
 {
-	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
-	{
-		Chams::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
-	}
+    if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
+    {
+        Chams::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
+    }
 
-	modelRenderVMT->GetOriginalMethod<DrawModelExecuteFn>(21)(thisptr, context, state, pInfo, pCustomBoneToWorld);
-	modelRender->ForcedMaterialOverride(NULL);
+    modelRenderVMT->GetOriginalMethod<DrawModelExecuteFn>(21)(thisptr, context, state, pInfo, pCustomBoneToWorld);
+    modelRender->ForcedMaterialOverride(NULL);
 
-	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
-	{
-		ESP::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
-	}
+    if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
+    {
+        ESP::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
+    }
 }
