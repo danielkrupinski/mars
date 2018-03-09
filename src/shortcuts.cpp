25cd6687ec2a0c2847cd19c9a1005dae34c58631
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 19:12:49 +0100

    Update formatting

--- src/shortcuts.cpp
+++ src/shortcuts.cpp
@@ -2,14 +2,14 @@
 
 void Shortcuts::PollEvent(SDL_Event* event)
 {
-	if (((event->key.keysym.sym == SDLK_INSERT && event->type == SDL_KEYDOWN)) || ((event->key.keysym.mod & KMOD_LALT) && event->key.keysym.sym == SDLK_i && event->type == SDL_KEYDOWN))
-		UI::SetVisible(!UI::isVisible);
+    if (((event->key.keysym.sym == SDLK_INSERT && event->type == SDL_KEYDOWN)) || ((event->key.keysym.mod & KMOD_LALT) && event->key.keysym.sym == SDLK_i && event->type == SDL_KEYDOWN))
+    UI::SetVisible(!UI::isVisible);
 }
 
 void Shortcuts::SetKeyCodeState(ButtonCode_t code, bool bPressed)
 {
-#ifdef EXPERIMENTAL_SETTINGS
-	if (inputSystem->IsButtonDown(ButtonCode_t::KEY_LALT) && code == ButtonCode_t::KEY_F && bPressed)
-		Settings::ThirdPerson::enabled = !Settings::ThirdPerson::enabled;
-#endif
+    #ifdef EXPERIMENTAL_SETTINGS
+    if (inputSystem->IsButtonDown(ButtonCode_t::KEY_LALT) && code == ButtonCode_t::KEY_F && bPressed)
+    Settings::ThirdPerson::enabled = !Settings::ThirdPerson::enabled;
+    #endif
 }
