fede917d7041a273e7c168f2ec8efe976d835fcc
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:43:52 +0100

    Update formatting

--- src/Hooks/GetViewModelFOV.cpp
+++ src/Hooks/GetViewModelFOV.cpp
@@ -2,10 +2,10 @@
 
 float Hooks::GetViewModelFOV(void* thisptr)
 {
-	float fov = clientModeVMT->GetOriginalMethod<GetViewModelFOVFn>(36)(thisptr);
+    float fov = clientModeVMT->GetOriginalMethod<GetViewModelFOVFn>(36)(thisptr);
 
-	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
-		FOVChanger::GetViewModelFOV(fov);
+    if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
+    FOVChanger::GetViewModelFOV(fov);
 
-	return fov;
+    return fov;
 }
