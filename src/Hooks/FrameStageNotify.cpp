980e4b20fcd88a9e3780283953c039af49e7d109
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:43:29 +0100

    Update formatting

--- src/Hooks/FrameStageNotify.cpp
+++ src/Hooks/FrameStageNotify.cpp
@@ -2,24 +2,24 @@
 
 void Hooks::FrameStageNotify(void* thisptr, ClientFrameStage_t stage)
 {
-	CustomGlow::FrameStageNotify(stage);
-	SkinChanger::FrameStageNotifyModels(stage);
-	SkinChanger::FrameStageNotifySkins(stage);
-	Noflash::FrameStageNotify(stage);
-	View::FrameStageNotify(stage);
-	Resolver::FrameStageNotify(stage);
-	SkyBox::FrameStageNotify(stage);
-	ASUSWalls::FrameStageNotify(stage);
-	NoSmoke::FrameStageNotify(stage);
+    CustomGlow::FrameStageNotify(stage);
+    SkinChanger::FrameStageNotifyModels(stage);
+    SkinChanger::FrameStageNotifySkins(stage);
+    Noflash::FrameStageNotify(stage);
+    View::FrameStageNotify(stage);
+    Resolver::FrameStageNotify(stage);
+    SkyBox::FrameStageNotify(stage);
+    ASUSWalls::FrameStageNotify(stage);
+    NoSmoke::FrameStageNotify(stage);
 
-	if (SkinChanger::forceFullUpdate)
-	{
-		GetLocalClient(-1)->m_nDeltaTick = -1;
-		SkinChanger::forceFullUpdate = false;
-	}
+    if (SkinChanger::forceFullUpdate)
+    {
+        GetLocalClient(-1)->m_nDeltaTick = -1;
+        SkinChanger::forceFullUpdate = false;
+    }
 
-	clientVMT->GetOriginalMethod<FrameStageNotifyFn>(36)(thisptr, stage);
+    clientVMT->GetOriginalMethod<FrameStageNotifyFn>(36)(thisptr, stage);
 
-	Resolver::PostFrameStageNotify(stage);
-	View::PostFrameStageNotify(stage);
+    Resolver::PostFrameStageNotify(stage);
+    View::PostFrameStageNotify(stage);
 }
