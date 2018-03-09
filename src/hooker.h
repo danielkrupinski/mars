eb6a3ecc16a6bd8df1c8538abf66d1dbe32a4703
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 19:07:21 +0100

    Update formatting

--- src/hooker.h
+++ src/hooker.h
@@ -14,36 +14,36 @@
 
 struct dlinfo_t
 {
-	const char* library = nullptr;
-	uintptr_t address = 0;
-	size_t size = 0;
+    const char* library = nullptr;
+    uintptr_t address = 0;
+    size_t size = 0;
 };
 
 namespace Hooker
 {
-	bool GetLibraryInformation(const char* library, uintptr_t* address, size_t* size);
-	void InitializeVMHooks();
-	bool HookRecvProp(const char* className, const char* propertyName, std::unique_ptr<RecvPropHook>& recvPropHook);
-	void FindIClientMode();
-	void FindGlobalVars();
-	void FindCInput();
-	void FindGlowManager();
-	void FindPlayerResource();
-	void FindGameRules();
-	void FindRankReveal();
-	void FindSendClanTag();
-	void FindViewRender();
-	void FindPrediction();
-	void FindIsReadyCallback();
-	void FindSurfaceDrawing();
-	void FindGetLocalClient();
-	void FindLineGoesThroughSmoke();
-	void FindInitKeyValues();
-	void FindLoadFromBuffer();
-	//void FindVstdlibFunctions();
-	void FindOverridePostProcessingDisable();
-	void HookSwapWindow();
-	void HookPollEvent();
-	void FindSDLInput();
-	void FindSetNamedSkybox();
+    bool GetLibraryInformation(const char* library, uintptr_t* address, size_t* size);
+    void InitializeVMHooks();
+    bool HookRecvProp(const char* className, const char* propertyName, std::unique_ptr<RecvPropHook>& recvPropHook);
+    void FindIClientMode();
+    void FindGlobalVars();
+    void FindCInput();
+    void FindGlowManager();
+    void FindPlayerResource();
+    void FindGameRules();
+    void FindRankReveal();
+    void FindSendClanTag();
+    void FindViewRender();
+    void FindPrediction();
+    void FindIsReadyCallback();
+    void FindSurfaceDrawing();
+    void FindGetLocalClient();
+    void FindLineGoesThroughSmoke();
+    void FindInitKeyValues();
+    void FindLoadFromBuffer();
+    //void FindVstdlibFunctions();
+    void FindOverridePostProcessingDisable();
+    void HookSwapWindow();
+    void HookPollEvent();
+    void FindSDLInput();
+    void FindSetNamedSkybox();
 }
