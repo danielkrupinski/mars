5844a27bdc37a9cfbc7eac80744fae86991f94f5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:52:46 +0100

    Update formatting

--- src/Hooks/SetMouseCodeState.cpp
+++ src/Hooks/SetMouseCodeState.cpp
@@ -2,12 +2,12 @@
 
 void Hooks::SetMouseCodeState(void* thisptr, ButtonCode_t code, MouseCodeState_t state)
 {
-	if (SetKeyCodeState::shouldListen && state == MouseCodeState_t::BUTTON_PRESSED)
-	{
-		SetKeyCodeState::shouldListen = false;
-		*SetKeyCodeState::keyOutput = code;
-		UI::UpdateWeaponSettings();
-	}
+    if (SetKeyCodeState::shouldListen && state == MouseCodeState_t::BUTTON_PRESSED)
+    {
+        SetKeyCodeState::shouldListen = false;
+        *SetKeyCodeState::keyOutput = code;
+        UI::UpdateWeaponSettings();
+    }
 
-	inputInternalVMT->GetOriginalMethod<SetMouseCodeStateFn>(93)(thisptr, code, state);
+    inputInternalVMT->GetOriginalMethod<SetMouseCodeStateFn>(93)(thisptr, code, state);
 }
