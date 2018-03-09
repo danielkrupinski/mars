d2284c29433b02783ab4d81927536f503a7a5e76
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 17:58:28 +0100

    Update formatting

--- src/SDK/CCSGameRules.h
+++ src/SDK/CCSGameRules.h
@@ -3,22 +3,22 @@
 class C_CSGameRules
 {
 public:
-	bool IsValveDS()
-	{
-		return *(bool*)((uintptr_t)this + offsets.DT_CSGameRulesProxy.m_bIsValveDS);
-	}
+    bool IsValveDS()
+    {
+        return *(bool*)((uintptr_t)this + offsets.DT_CSGameRulesProxy.m_bIsValveDS);
+    }
 
-	bool IsBombDropped()
-	{
-		return *(bool*)((uintptr_t)this + offsets.DT_CSGameRulesProxy.m_bBombDropped);
-	}
+    bool IsBombDropped()
+    {
+        return *(bool*)((uintptr_t)this + offsets.DT_CSGameRulesProxy.m_bBombDropped);
+    }
 
-	bool IsBombPlanted()
-	{
-		return *(bool*)((uintptr_t)this + offsets.DT_CSGameRulesProxy.m_bBombPlanted);
-	}
-	bool IsFreezeTime()
-	{
-		return *(bool *) ((uintptr_t) this + offsets.DT_CSGameRulesProxy.m_bFreezePeriod);
-	}
+    bool IsBombPlanted()
+    {
+        return *(bool*)((uintptr_t)this + offsets.DT_CSGameRulesProxy.m_bBombPlanted);
+    }
+    bool IsFreezeTime()
+    {
+        return *(bool *) ((uintptr_t) this + offsets.DT_CSGameRulesProxy.m_bFreezePeriod);
+    }
 };
