0abd99df930d503c976aaf40edf50efd3879dd44
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:50:30 +0100

    Update formatting

--- src/Hooks/PumpWindowsMessageLoop.cpp
+++ src/Hooks/PumpWindowsMessageLoop.cpp
@@ -2,8 +2,8 @@
 
 int Hooks::PumpWindowsMessageLoop(void* thisptr, void* unknown)
 {
-	if (UI::isVisible && !SetKeyCodeState::shouldListen)
-		return 0;
+    if (UI::isVisible && !SetKeyCodeState::shouldListen)
+    return 0;
 
-	return launcherMgrVMT->GetOriginalMethod<PumpWindowsMessageLoopFn>(19)(thisptr, unknown);
+    return launcherMgrVMT->GetOriginalMethod<PumpWindowsMessageLoopFn>(19)(thisptr, unknown);
 }
