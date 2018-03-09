c5d9c6761c40348f5e52ea51bb32b7e1b9703716
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 17:40:14 +0100

    Update formatting

--- src/ATGUI/atgui.h
+++ src/ATGUI/atgui.h
@@ -26,23 +26,23 @@
 
 namespace UI
 {
-	extern bool isVisible;
+    extern bool isVisible;
 
-	void SetVisible(bool visible);
-	void SwapWindow();
-	void SetupColors();
-	void SetupWindows();
-	bool ColorPicker(float* col, bool alphabar);
-	bool ColorPicker3(float col[3]);
-	bool ColorPicker4(float col[4]);
-	void KeyBindButton(ButtonCode_t* key);
-	void UpdateWeaponSettings();
-	void ReloadWeaponSettings();
+    void SetVisible(bool visible);
+    void SwapWindow();
+    void SetupColors();
+    void SetupWindows();
+    bool ColorPicker(float* col, bool alphabar);
+    bool ColorPicker3(float col[3]);
+    bool ColorPicker4(float col[4]);
+    void KeyBindButton(ButtonCode_t* key);
+    void UpdateWeaponSettings();
+    void ReloadWeaponSettings();
 }
 
 
 inline void SetTooltip(const char* text)
 {
-	if (ImGui::IsItemHovered())
-		ImGui::SetTooltip("%s", text);
+    if (ImGui::IsItemHovered())
+    ImGui::SetTooltip("%s", text);
 }
