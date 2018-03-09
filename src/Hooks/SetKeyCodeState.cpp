cfdbd50979e5fc699ce4bb7ce63472dd7c1895d3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 16:52:19 +0100

    Update formatting

--- src/Hooks/SetKeyCodeState.cpp
+++ src/Hooks/SetKeyCodeState.cpp
@@ -5,15 +5,15 @@ ButtonCode_t* SetKeyCodeState::keyOutput = nullptr;
 
 void Hooks::SetKeyCodeState(void* thisptr, ButtonCode_t code, bool bPressed)
 {
-	if (SetKeyCodeState::shouldListen && bPressed)
-	{
-		SetKeyCodeState::shouldListen = false;
-		*SetKeyCodeState::keyOutput = code;
-		UI::UpdateWeaponSettings();
-	}
+    if (SetKeyCodeState::shouldListen && bPressed)
+    {
+        SetKeyCodeState::shouldListen = false;
+        *SetKeyCodeState::keyOutput = code;
+        UI::UpdateWeaponSettings();
+    }
 
-	if (!SetKeyCodeState::shouldListen)
-		Shortcuts::SetKeyCodeState(code, bPressed);
+    if (!SetKeyCodeState::shouldListen)
+    Shortcuts::SetKeyCodeState(code, bPressed);
 
-	inputInternalVMT->GetOriginalMethod<SetKeyCodeStateFn>(92)(thisptr, code, bPressed);
+    inputInternalVMT->GetOriginalMethod<SetKeyCodeStateFn>(92)(thisptr, code, bPressed);
 }
