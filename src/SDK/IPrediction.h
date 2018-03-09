14833beb23994472b8eb90913c6559471b7ab7a2
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 18:17:48 +0100

    Update formatting

--- src/SDK/IPrediction.h
+++ src/SDK/IPrediction.h
@@ -3,15 +3,15 @@
 class IPrediction
 {
 public:
-	void SetupMove(IClientEntity* player, CUserCmd* cmd, IMoveHelper* helper, CMoveData* move) 
-	{
-		typedef void (* oSetupMove)(void*, IClientEntity*, CUserCmd*, IMoveHelper*, CMoveData*);
-		return getvfunc<oSetupMove>(this, 21)(this, player, cmd, helper, move);
-	}
+    void SetupMove(IClientEntity* player, CUserCmd* cmd, IMoveHelper* helper, CMoveData* move)
+    {
+        typedef void (* oSetupMove)(void*, IClientEntity*, CUserCmd*, IMoveHelper*, CMoveData*);
+        return getvfunc<oSetupMove>(this, 21)(this, player, cmd, helper, move);
+    }
 
-	void FinishMove(IClientEntity* player, CUserCmd* cmd, CMoveData* move)
-	{
-		typedef void (* oFinishMove)(void*, IClientEntity*, CUserCmd*, CMoveData*);
-		return getvfunc<oFinishMove>(this, 22)(this, player, cmd, move);
-	}
+    void FinishMove(IClientEntity* player, CUserCmd* cmd, CMoveData* move)
+    {
+        typedef void (* oFinishMove)(void*, IClientEntity*, CUserCmd*, CMoveData*);
+        return getvfunc<oFinishMove>(this, 22)(this, player, cmd, move);
+    }
 };
