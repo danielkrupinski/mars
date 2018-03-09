edb2c2d550c8f9e7b44916488bf51cebe2d9a547
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-09 17:59:55 +0100

    Update formatting

--- src/SDK/CGlobalVars.h
+++ src/SDK/CGlobalVars.h
@@ -3,22 +3,22 @@
 class CGlobalVars
 {
 public:
-	float realtime;
-	int framecount;
-	float absoluteframetime;
-	float absoluteframestarttimestddev;
-	float curtime;
-	float frametime;
-	int maxClients;
-	int tickcount;
-	float interval_per_tick;
-	float interpolation_amount;
-	int simTicksThisFrame;
-	int network_protocol;
-	void* pSaveData;
-	bool m_bClient;
-	bool m_bRemoteClient;
+    float realtime;
+    int framecount;
+    float absoluteframetime;
+    float absoluteframestarttimestddev;
+    float curtime;
+    float frametime;
+    int maxClients;
+    int tickcount;
+    float interval_per_tick;
+    float interpolation_amount;
+    int simTicksThisFrame;
+    int network_protocol;
+    void* pSaveData;
+    bool m_bClient;
+    bool m_bRemoteClient;
 private:
-	int nTimestampNetworkingBase;
-	int nTimestampRandomizeWindow;
+    int nTimestampNetworkingBase;
+    int nTimestampRandomizeWindow;
 };
