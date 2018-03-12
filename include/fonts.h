897a51521e39a84cf3a48c60e17ff13f0efd8d36
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-12 17:49:12 +0100

    Update source code

--- include/fonts.h
+++ include/fonts.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef FONTS_H
+#define FONTS_H
 
 #include "settings.h"
 
 extern HFont esp_font;
 
-namespace Fonts
-{
+namespace Fonts {
     void SetupFonts();
 }
+
+#endif // FONTS_H
