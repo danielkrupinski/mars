07350dd9273c4a4ace04fb271799e604bb4f4684
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-14 19:30:58 +0100

    Update source code

--- include/preload.h
+++ include/preload.h
@@ -3,20 +3,20 @@
 
 #include "Fuzion.h"
 extern "C" {
-    extern char **environ;
+    extern char** environ;
 }
 
 extern int (*orig_open)(const char*, int oflag);
-extern char* (*orig_getenv)(const char *);
-extern int (*orig_execve)(const char *path, char *const argv[], char *const envp[]);
+extern char* (*orig_getenv)(const char*);
+extern int (*orig_execve)(const char* path, char *const argv[], char *const envp[]);
 extern FILE* (*orig_fopen)(const char*, const char*);
 
 extern "C" {
-    char* getenv(const char *name);
-    int execve(const char *path, char *const argv[], char *const envp[]);
+    char* getenv(const char* name);
+    int execve(const char* path, char *const argv[], char *const envp[]);
 
-    int open(const char *path, int oflag, ...);
-    FILE* fopen(const char *path, const char *mode);
+    int open(const char* path, int oflag, ...);
+    FILE* fopen(const char* path, const char* mode);
 }
 
 
