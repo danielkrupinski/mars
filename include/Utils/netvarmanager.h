74c9249ec5520540accaca4463961f68de02e85f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-15 17:26:16 +0100

    Source code optimization

--- include/Utils/netvarmanager.h
+++ include/Utils/netvarmanager.h
@@ -15,15 +15,14 @@
 #include "../SDK/SDK.h"
 #include "../interfaces.h"
 
-namespace NetVarManager
-{
-    std::vector<RecvTable*> GetTables();
-    RecvTable* GetTable(std::vector<RecvTable*> tables, const char* tableName);
-    int GetOffset(std::vector<RecvTable*> tables, const char* tableName, const char *propName);
-    int GetProp(std::vector<RecvTable*> tables, const char* tableName, const char* propName, RecvProp** prop = 0);
-    int GetProp(std::vector<RecvTable*> tables, RecvTable* recvTable, const char* propName, RecvProp** prop = 0);
-    std::string DumpTable(RecvTable* table, int depth);
-    bool HookProp(const char* tableName, const char* propName, RecvVarProxyFn f);
+namespace NetVarManager {
+    std::vector<RecvTable*> GetTables;
+    RecvTable* GetTable(std::vector<RecvTable*>, const char*);
+    int GetOffset(std::vector<RecvTable*>, const char*, const char*);
+    int GetProp(std::vector<RecvTable*>, const char*, const char*, RecvProp** = 0);
+    int GetProp(std::vector<RecvTable*>, RecvTable*, const char*, RecvProp** = 0);
+    std::string DumpTable(RecvTable*, int);
+    bool HookProp(const char*, const char*, RecvVarProxyFn);
     void DumpNetvars();
 }
 
