e519ae2e9596935b1af8339bcdffa7151677b2fa
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-15 17:33:21 +0100

    Add header guard

--- include/Utils/patternfinder.h
+++ include/Utils/patternfinder.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef PATTERN_FINDER_H
+#define PATTERN_FINDER_H
 
 #include <cstdint>
 
@@ -7,3 +8,5 @@ namespace PatternFinder
     uintptr_t FindPattern(uintptr_t dwAddress, uintptr_t dwLen, unsigned char* bMask, const char* szMask);
     uintptr_t FindPatternInModule(const char* moduleName, unsigned char* bMask, const char* szMask);
 }
+
+#endif PATTERN_FINDER_H
