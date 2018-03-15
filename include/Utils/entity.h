3f5865ab880b3cb85eda00dbb98be89f495f94f5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-15 17:11:19 +0100

    Remove arguments' names from functions parametters

--- include/Utils/entity.h
+++ include/Utils/entity.h
@@ -7,12 +7,12 @@
 
 namespace Entity
 {
-    bool IsVisible(C_BasePlayer* player, int bone, float fov = 180.f, bool smoke_check = false);
-    bool IsSpotVisible(C_BasePlayer* player, Vector spot, float fov = 180.f, bool smoke_check = false);
-    bool IsVisibleThroughEnemies(C_BasePlayer *player, int bone, float fov = 180.f, bool smoke_check = false);
-    bool IsSpotVisibleThroughEnemies(C_BasePlayer *player, Vector spot, float fov = 180.f, bool smoke_check = false);
-    bool IsPlanting(C_BasePlayer* player);
-    Bone GetBoneByName(C_BasePlayer* player, const char* boneName);
+    bool IsVisible(C_BasePlayer*, int, float = 180.f, bool = false);
+    bool IsSpotVisible(C_BasePlayer*, Vector, float = 180.f, bool = false);
+    bool IsVisibleThroughEnemies(C_BasePlayer*, int, float = 180.f, bool = false);
+    bool IsSpotVisibleThroughEnemies(C_BasePlayer*, Vector, float = 180.f, bool = false);
+    bool IsPlanting(C_BasePlayer*);
+    Bone GetBoneByName(C_BasePlayer*, const char*);
 }
 
 #endif // ENTITY_H
