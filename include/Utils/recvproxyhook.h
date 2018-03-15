193a29eef6e27146008969880c12ac9e453b2000
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-15 17:42:09 +0100

    Update header

--- include/Utils/recvproxyhook.h
+++ include/Utils/recvproxyhook.h
@@ -10,25 +10,10 @@ private:
     RecvVarProxyFn original_proxy_fn;
 public:
     RecvPropHook(RecvProp* target_property)
-    {
-        this->target_property = target_property;
-        this->original_proxy_fn = target_property->m_ProxyFn;
-    }
-
     ~RecvPropHook()
-    {
-        target_property->m_ProxyFn = this->original_proxy_fn;
-    }
 
     RecvVarProxyFn GetOriginalFunction()
-    {
-        return this->original_proxy_fn;
-    }
-
     void SetProxyFunction(RecvVarProxyFn user_proxy_fn)
-    {
-        target_property->m_ProxyFn = user_proxy_fn;
-    }
 };
 
 #endif // RECVPROXY_HOOK_H
