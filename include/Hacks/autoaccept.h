36023b7273812f05a4093287f0752260f8287b81
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-17 10:01:48 +0100

    Add header guard

--- include/Hacks/autoaccept.h
+++ include/Hacks/autoaccept.h
@@ -1,12 +1,14 @@
-#pragma once
+#ifndef AUTOACCEPT_H
+#define AUTOACCEPT_H
 
 #include "../interfaces.h"
 #include "../settings.h"
 
-namespace AutoAccept
-{
+namespace AutoAccept {
     //Hooks
-    void PlaySound(const char* filename);
+    void PlaySound(const char*);
 }
 
 extern IsReadyCallbackFn IsReadyCallback;
+
+#endif // AUTOACCEPT_H
