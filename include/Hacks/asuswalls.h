196205049e36abc2056c3a8bc2de0280d100c66b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-17 10:00:25 +0100

    Add header guard

--- include/Hacks/asuswalls.h
+++ include/Hacks/asuswalls.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef ASUSWALLS_H
+#define ASUSWALLS_H
 
 #include "../interfaces.h"
 #include "../settings.h"
 
-namespace ASUSWalls
-{
+namespace ASUSWalls {
     //Hooks
-    void FrameStageNotify(ClientFrameStage_t stage);
+    void FrameStageNotify(ClientFrameStage_t);
 }
+
+#endif // ASUSWALLS_H
