072caf3ea306eeb4612d07c9ea44b2fae1223265
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 13:04:39 +0100

    Add header guard

--- include/ATGUI/Windows/skinmodelchanger.h
+++ include/ATGUI/Windows/skinmodelchanger.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef SKINMODEL_CHANGER_H
+#define SKINMODEL_CHANGER_H
 
 #include "../atgui.h"
 
-namespace SkinModelChanger
-{
+namespace SkinModelChanger {
     extern bool showWindow;
 
     extern void RenderWindow();
 }
+
+#endif // SKINMODEL_CHANGER_H
