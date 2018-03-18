f5dd2f132242beaeca07c9ffdd3dd35fc331fb62
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:16:07 +0100

    Add header guard

--- include/Hacks/fakelag.h
+++ include/Hacks/fakelag.h
@@ -1,12 +1,14 @@
-#pragma once
+#ifndef FAKELAG_H
+#define FAKELAG_H
 
 #include "../settings.h"
 #include "../interfaces.h"
 #include "../Hooks/hooks.h"
 #include "../Utils/util.h"
 
-namespace FakeLag
-{
+namespace FakeLag {
     //Hooks
-    void CreateMove(CUserCmd* cmd);
+    void CreateMove(CUserCmd*);
 };
+
+#endif // FAKELAG_H
