e24289a9ab76fe6d65cdefd573991e05c2f13626
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:48:49 +0100

    Add header guard

--- include/Hacks/hitmarkers.h
+++ include/Hacks/hitmarkers.h
@@ -1,12 +1,14 @@
-#pragma once
+#ifndef HITMARKERS_H
+#define HITMARKERS_H
 
 #include "../Utils/draw.h"
 #include "../interfaces.h"
 #include "../Utils/entity.h"
 
-namespace Hitmarkers
-{
+namespace Hitmarkers {
     //Hooks
-    void FireGameEvent(IGameEvent* event);
+    void FireGameEvent(IGameEvent*);
     void Paint();
 }
+
+#endif // HITMARKERS_H
