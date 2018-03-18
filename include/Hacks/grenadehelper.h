230e5fa41526e16a6e994ff1ea1013ae826adab8
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:46:28 +0100

    Add header guard

--- include/Hacks/grenadehelper.h
+++ include/Hacks/grenadehelper.h
@@ -1,23 +1,17 @@
-#pragma once
+#ifndef GRENADEHELPER_H
+#define GRENADEHELPER_H
 
 #include "../SDK/SDK.h"
 #include "../settings.h"
 #include <algorithm>
 
 
-GrenadeType GetGrenadeType(C_BaseCombatWeapon *wpn);
+GrenadeType GetGrenadeType(C_BaseCombatWeapon*);
 
-namespace GrenadeHelper
-{
-    /*
-    void DrawGrenadeInfo(GrenadeInfo* info);
-    void DrawAimHelp(GrenadeInfo* info);
-    void AimAssist(CUserCmd* cmd);
-    ImColor GetColor(GrenadeType type);
-    void CheckForUpdate();
-    */
-
-    //Hooks
-    void CreateMove(CUserCmd* cmd);//Actively Help to throw grenade
-    void Paint();//Draw the Grenade Helper Esp
+namespace GrenadeHelper {
+    // Hooks
+    void CreateMove(CUserCmd*); // Actively Help to throw grenade
+    void Paint(); // Draw the Grenade Helper Esp
 }
+
+#endif // GRENADEHELPER_H
