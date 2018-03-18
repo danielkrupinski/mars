34dcd5876d021ff48814cf6018210addea4931f3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 12:07:02 +0100

    Add header

--- include/Hacks/showspectators.h
+++ include/Hacks/showspectators.h
@@ -1,11 +1,13 @@
-#pragma once
+#ifndef SHOWSPECTATORS_H
+#define SHOWSPECTATORS_H
 
 #include "../settings.h"
 #include "../interfaces.h"
 #include "../Utils/entity.h"
 
-namespace ShowSpectators
-{
-    std::list<int> GetObservervators(int playerId);
+namespace ShowSpectators {
+    std::list<int> GetObservervators(int);
     void RenderWindow();
 }
+
+#endif // SHOWSPECTATORS_H
