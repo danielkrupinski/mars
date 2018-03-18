6b9406a2b2ae018fe8d70bf464194988a72fb93b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 12:01:39 +0100

    Add header guard

--- include/Hacks/predictionsystem.h
+++ include/Hacks/predictionsystem.h
@@ -1,12 +1,14 @@
-#pragma once
+#ifndef PREDICTIONSYSTEM_H
+#define PREDICTIONSYSTEM_H
 
 #include "../settings.h"
 #include "../SDK/SDK.h"
 
-namespace PredictionSystem
-{
+namespace PredictionSystem {
     //Hooks
     /* CreateMove */
-    void StartPrediction(CUserCmd* cmd);
+    void StartPrediction(CUserCmd*);
     void EndPrediction();
-};
+}
+
+#endif // PREDICTIONSYSTEM_H
