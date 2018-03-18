dcf33b3ff46586b8e334d751260dfbc4f6d0991e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:56:45 +0100

    Add header guard

--- include/Hacks/namestealer.h
+++ include/Hacks/namestealer.h
@@ -1,13 +1,15 @@
-#pragma once
+#ifndef NAMESTEALER_H
+#define NAMESTEALER_H
 
 #include "../SDK/SDK.h"
 #include "../settings.h"
 
-namespace NameStealer
-{
+namespace NameStealer {
     extern int entityId;
 
     //Hooks
-    void BeginFrame(float frameTime);
-    void FireGameEvent(IGameEvent* event);
+    void BeginFrame(float);
+    void FireGameEvent(IGameEvent*);
 };
+
+#endif // NAMESTEALER_H
