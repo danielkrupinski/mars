8eb4f202063e9d90a3c97fd34ab397a7a3da74db
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:04:04 +0100

    Add header guard

--- include/Hacks/dlights.h
+++ include/Hacks/dlights.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef DLIGHTS_H
+#define DLIGHTS_H
 
 #include "../settings.h"
 #include "../interfaces.h"
 
-namespace Dlights
-{
+namespace Dlights {
     //Hooks
     void Paint();
 }
+
+#endif // DLIGHTS_H
