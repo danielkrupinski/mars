4461fb60f31f30a8eedc0e2a124367927ac91e02
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 12:04:04 +0100

    Add header guard

--- include/Hacks/recoilcrosshair.h
+++ include/Hacks/recoilcrosshair.h
@@ -1,11 +1,13 @@
-#pragma once
+#ifndef RECOIL_CROSSHAIR_H
+#define RECOIL_CROSSHAIR_H
 
 #include "../settings.h"
 #include "../Utils/draw.h"
 #include "../interfaces.h"
 
-namespace Recoilcrosshair
-{
+namespace Recoilcrosshair {
     //Hooks
     void Paint();
-};
+}
+
+#endif // RECOIL_CROSSHAIR_H
