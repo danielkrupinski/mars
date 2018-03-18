d67c0e02a1f1531e8c1cfc49d4d03247440f9e14
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:11:01 +0100

    Add header guard and update header

--- include/Hacks/esp.h
+++ include/Hacks/esp.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ESP_H
+#define ESP_H
 
 #include <math.h>
 #include "../settings.h"
@@ -7,45 +8,17 @@
 #include "../interfaces.h"
 #include "../Utils/entity.h"
 
-namespace ESP
-{
+namespace ESP {
     extern const char* ranks[];
 
-    ImColor GetESPPlayerColor(C_BasePlayer* player, bool visible);
-
-
-
-
-    /*
-    bool GetBox(C_BaseEntity* entity, int& x, int& y, int& w, int& h);
-    void DrawAutoWall(C_BasePlayer* player);
-    void DrawBox(Color color, int x, int y, int w, int h, C_BaseEntity* entity);
-    void DrawEntity(C_BaseEntity* entity, const char* string, Color color);
-    void DrawPlayer(int index, C_BasePlayer* player, IEngineClient::player_info_t player_info);
-    void DrawBomb(C_BaseCombatWeapon* bomb);
-    void DrawPlantedBomb(C_PlantedC4* bomb);
-    void DrawDefuseKit(C_BaseEntity* defuser);
-    void DrawDroppedWeapons(C_BaseCombatWeapon* weapon);
-    void DrawHostage(C_BaseEntity* hostage);
-    void DrawChicken(C_BaseEntity* chicken);
-    void DrawFish(C_BaseEntity* fish);
-    void DrawThrowable(C_BaseEntity* throwable, ClientClass* cClass);
-    void DrawSkeleton(C_BasePlayer* player);
-    void DrawBulletTrace(C_BasePlayer* player);
-    void DrawTracer(C_BasePlayer* player);
-    void DrawHeaddot(C_BasePlayer* player);
-    void CollectFootstep(int iEntIndex, const char *pSample);
-    void DrawSounds();
-    void DrawSpread();
-    void DrawFOVCrosshair();
-    void DrawGlow();
-    void DrawScope();
-    */
+    ImColor GetESPPlayerColor(C_BasePlayer*, bool);
 
     //Hooks
-    void DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld);
-    bool PrePaintTraverse(VPANEL vgui_panel, bool force_repaint, bool allow_force);
+    void DrawModelExecute(void*, void*, void*, const ModelRenderInfo_t&, matrix3x4_t*);
+    bool PrePaintTraverse(VPANEL, bool, bool);
     void Paint();
-    void EmitSound(int iEntIndex, const char *pSample);
-    void CreateMove(CUserCmd* cmd);
+    void EmitSound(int, const char*);
+    void CreateMove(CUserCmd*);
 }
+
+#endif // ESP_H
