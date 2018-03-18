fb89cbc033c120a9cb0d0b1591fe1e2010f5651d
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 11:00:36 +0100

    Add header guard

--- include/Hacks/customglow.h
+++ include/Hacks/customglow.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef CUSTOMGLOW_H
+#define CUSTOMGLOW_H
 
 #include "../interfaces.h"
 #include "../settings.h"
 
-namespace CustomGlow
-{
+namespace CustomGlow {
     //Hooks
-    void FrameStageNotify(ClientFrameStage_t stage);
+    void FrameStageNotify(ClientFrameStage_t);
 }
+
+#endif CUSTOMGLOW_H
