6d8cd81f7eb617b40cc56384a63dd8c6760da712
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 12:12:41 +0100

    Add header guard

--- include/Hacks/snipercrosshair.h
+++ include/Hacks/snipercrosshair.h
@@ -1,11 +1,12 @@
-#pragma once
+#ifndef SNIPER_CROSSHAIR_H
+#define SNIPER_CROSSHAIR_H
 
 #include "../interfaces.h"
 #include "../settings.h"
 
-namespace SniperCrosshair
-{
-
+namespace SniperCrosshair {
     //Hooks
     void Paint();
-};
+}
+
+#endif // SNIPER_CROSSHAIR_H
