15a6f178fd794296d085983b378fa6aa3af3de67
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 12:16:42 +0100

    Add header guard

--- include/Hacks/tracereffect.h
+++ include/Hacks/tracereffect.h
@@ -1,13 +1,14 @@
-#pragma once
+#ifndef TRACER_EFFECT_H
+#define TRACER_EFFECT_H
 
 #include "../interfaces.h"
 #include "../settings.h"
 #include <map>
 #include <string>
 #include <vector>
-namespace TracerEffect
-{
+
+namespace TracerEffect {
     void RestoreTracers();
     //Hooks
-    void CreateMove(CUserCmd* cmd);
+    void CreateMove(CUserCmd*);
 }
