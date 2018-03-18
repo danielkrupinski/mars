3cc8784d95d69902706cfccaf727ebf77cc22c70
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 10:25:18 +0100

    Add header guard and update indentation

--- include/Hacks/autodefuse.h
+++ include/Hacks/autodefuse.h
@@ -1,12 +1,14 @@
-#pragma once
+#ifndef AUTODEFUSE_H
+#define AUTODEFUSE_H
 
 #include "../settings.h"
 #include "../interfaces.h"
 #include "../Utils/entity.h"
 #include "../Utils/math.h"
 
-namespace AutoDefuse
-{
+namespace AutoDefuse {
     //Hooks
     void CreateMove(CUserCmd* cmd);
 };
+
+#endif // AUTODEFUSE_H
