b7a5f785f44f5f7e39cf8acdea8630254a04a5a7
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-18 13:03:41 +0100

    Add header guard

--- include/ATGUI/Windows/playerlist.h
+++ include/ATGUI/Windows/playerlist.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef PLAYERLIST_H
+#define PLAYERLIST_H
 
 #include "../atgui.h"
 
-namespace PlayerList
-{
+namespace PlayerList {
     extern bool showWindow;
 
     extern void RenderWindow();
 }
+
+#endif // PLAYERLIST_H
