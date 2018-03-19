55862260583dcbe9458ec84d108e36046f2620a8
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-19 16:43:42 +0100

    Add header guard

--- include/SDK/color.h
+++ include/SDK/color.h
@@ -1,9 +1,9 @@
-#pragma once
+#ifndef COLOR_H
+#define COLOR_H
 
 #include "../../lib/ImGUI/imgui.h"
 
-struct Color
-{
+struct Color {
     int r;
     int g;
     int b;
@@ -68,48 +68,42 @@ struct Color
         float q = brightness * (1.0f - saturation * f);
         float t = brightness * (1.0f - (saturation * (1.0f - f)));
 
-        if (h < 1)
-        {
+        if (h < 1) {
             return Color(
                 (unsigned char)(brightness * 255),
                 (unsigned char)(t * 255),
                 (unsigned char)(p * 255)
             );
         }
-        else if (h < 2)
-        {
+        else if (h < 2) {
             return Color(
                 (unsigned char)(q * 255),
                 (unsigned char)(brightness * 255),
                 (unsigned char)(p * 255)
             );
         }
-        else if (h < 3)
-        {
+        else if (h < 3) {
             return Color(
                 (unsigned char)(p * 255),
                 (unsigned char)(brightness * 255),
                 (unsigned char)(t * 255)
             );
         }
-        else if (h < 4)
-        {
+        else if (h < 4) {
             return Color(
                 (unsigned char)(p * 255),
                 (unsigned char)(q * 255),
                 (unsigned char)(brightness * 255)
             );
         }
-        else if (h < 5)
-        {
+        else if (h < 5) {
             return Color(
                 (unsigned char)(t * 255),
                 (unsigned char)(p * 255),
                 (unsigned char)(brightness * 255)
             );
         }
-        else
-        {
+        else {
             return Color(
                 (unsigned char)(brightness * 255),
                 (unsigned char)(p * 255),
@@ -138,3 +132,5 @@ struct Color
         );
     }
 };
+
+#endif // COLOR_H
