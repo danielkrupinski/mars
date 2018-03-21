a9c7373d27499ff12a3159a2e72618e03836953f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-21 18:14:29 +0100

    Remove airstuck calls

--- src/ATGUI/Tabs/misctab.cpp
+++ src/ATGUI/Tabs/misctab.cpp
@@ -405,7 +405,6 @@ void Misc::RenderTab()
                         ImGui::Checkbox(XORSTR("Fake Lag"), &Settings::FakeLag::enabled);
                         ImGui::Checkbox(XORSTR("Adaptive Fake Lag"), &Settings::FakeLag::adaptive);
                         ImGui::Checkbox(XORSTR("Auto Accept"), &Settings::AutoAccept::enabled);
-                        ImGui::Checkbox(XORSTR("AirStuck"), &Settings::Airstuck::enabled);
                         ImGui::Checkbox(XORSTR("Autoblock"), &Settings::Autoblock::enabled);
                         ImGui::Checkbox(XORSTR("Jump Throw"), &Settings::JumpThrow::enabled);
                         ImGui::Checkbox(XORSTR("Auto Defuse"), &Settings::AutoDefuse::enabled);
@@ -419,7 +418,6 @@ void Misc::RenderTab()
                         ImGui::PopItemWidth();
                         ImGui::Checkbox(XORSTR("Show Ranks"), &Settings::ShowRanks::enabled);
                         ImGui::Checkbox(XORSTR("Screenshot Cleaner"), &Settings::ScreenshotCleaner::enabled);
-                        UI::KeyBindButton(&Settings::Airstuck::key);
                         UI::KeyBindButton(&Settings::Autoblock::key);
                         UI::KeyBindButton(&Settings::JumpThrow::key);
                         ImGui::Checkbox(XORSTR("Silent Defuse"), &Settings::AutoDefuse::silent);
