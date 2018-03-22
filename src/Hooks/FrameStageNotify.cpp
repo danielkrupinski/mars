37060995a2a18d495eefa46a148466802549fcaa
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-22 16:16:41 +0100

    Remove resolver

--- src/Hooks/FrameStageNotify.cpp
+++ src/Hooks/FrameStageNotify.cpp
@@ -19,6 +19,5 @@ void Hooks::FrameStageNotify(void* thisptr, ClientFrameStage_t stage)
 
     clientVMT->GetOriginalMethod<FrameStageNotifyFn>(36)(thisptr, stage);
 
-    Resolver::PostFrameStageNotify(stage);
     View::PostFrameStageNotify(stage);
 }
