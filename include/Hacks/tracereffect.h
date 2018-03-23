a98ff03b4a91e7875f74b29561ea3ce56430791c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-23 19:10:32 +0100

    Revert "Remove tracereffect.h"
    This reverts commit 01f737fdd4d18a0ea24f49bb0930fc0c4c0eaf1d.

---
+++ include/Hacks/tracereffect.h
@@ -0,0 +1,16 @@
+#ifndef TRACER_EFFECT_H
+#define TRACER_EFFECT_H
+
+#include "../interfaces.h"
+#include "../settings.h"
+#include <map>
+#include <string>
+#include <vector>
+
+namespace TracerEffect {
+    void RestoreTracers();
+    //Hooks
+    void CreateMove(CUserCmd*);
+}
+
+#endif // TRACER_EFFECT_H
